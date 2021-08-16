6bb24edde5d466ac46f0c7916d034cdd3da70b1d
hadi gunawan <jtgunawan007@gmail.com>
2021-08-16 12:39:17 +0700

    menambah tabel admin dan seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         \App\Models\Admin::factory()->create();
-        
+        //
     }
 }
